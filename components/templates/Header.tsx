390e3d5e143d8e8a00be24c92c7ab8d96abb4f91
Takuya-wool-Sato <takuya@hab-co.jp>
2022-03-20 22:59:25 +0900

    icon追加

--- components/templates/Header.tsx
+++ components/templates/Header.tsx
@@ -1,6 +1,7 @@
 import { memo, FC } from 'react'
 import { Flex, Box, Heading, Spacer, Button } from '@chakra-ui/react'
 import NextLink from "next/link"
+import Image from 'next/image'
 
 // eslint-disable-next-line react/display-name
 export const Header: FC = memo(() => {
@@ -17,9 +18,14 @@ export const Header: FC = memo(() => {
       >
         <Box p='2'>
           <Heading size='md'>
-            <NextLink href='/' passHref>
-              Next.js study
-            </NextLink>
+            <Flex align={"center"}>
+              <NextLink href='/' passHref>
+                Next.js study
+              </NextLink>
+              <Box ml={"1"}>
+                <Image src="/logo.svg" alt="Vercel Logo" width={24} height={24} />
+              </Box>
+            </Flex>
           </Heading>
         </Box>
         <Spacer />
